2430e9802ea5afb6435d4c230f9b83f219bc115a
Aklilu Mandefro <aklilu.mandefro@volunteer-tech.com>
2023-05-30 02:27:40 -0700

    index.css, login and registration js files added

---
+++ src/Login.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import axios from "axios";
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
+
+export default function Login() {
+  // initial state
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [login, setLogin] = useState(false);
+
+  const handleSubmit = (e) => {
+    // prevent the form from refreshing the whole page
+    e.preventDefault();
+
+    // set configurations
+    const configuration = {
+      method: "post",
+      url: "https://nodejs-mongodb-auth-app.herokuapp.com/login",
+      data: {
+        email,
+        password,
+      },
+    };
+
+    // make the API call
+    axios(configuration)
+      .then((result) => {
+        // set the cookie
+        cookies.set("TOKEN", result.data.token, {
+          path: "/",
+        });
+        // redirect user to the auth page
+        window.location.href = "/auth";
+
+        setLogin(true);
+      })
+      .catch((error) => {
+        error = new Error();
+      });
+  };
+
+  return (
+    <>
+      <h2>Login</h2>
+      <Form onSubmit={(e) => handleSubmit(e)}>
+        {/* email */}
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter email"
+          />
+        </Form.Group>
+
+        {/* password */}
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+        </Form.Group>
+
+        {/* submit button */}
+        <Button
+          variant="primary"
+          type="submit"
+          onClick={(e) => handleSubmit(e)}
+        >
+          Login
+        </Button>
+
+        {/* display success message */}
+        {login ? (
+          <p className="text-success">You Are Logged in Successfully</p>
+        ) : (
+          <p className="text-danger">You Are Not Logged in</p>
+        )}
+      </Form>
+    </>
+  );
+}
