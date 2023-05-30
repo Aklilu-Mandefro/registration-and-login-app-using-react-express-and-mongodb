78e0f882233b5006421d26ddd781f95cc46ca6c5
Aklilu Mandefro <aklilu.mandefro@volunteer-tech.com>
2023-05-30 02:16:12 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# React Registration and Login App using React, Express and MongoDB
+
+
+## Dependencies
+* [Axios](https://www.npmjs.com/package/axios)
+* [React-Bootstrap](https://react-bootstrap.github.io/)
+* [universal-cookie](https://www.npmjs.com/package/universal-cookie)
+* [react-router-dom](https://www.npmjs.com/package/react-router-dom)
+
+## Installation
+* Clone the whole project or by branch name
+* Run ``npm install``
+* Run ``npm start``
+*That will open the project on your default browser*
---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { Switch, Route } from "react-router-dom";
+import { Container, Col, Row } from "react-bootstrap";
+import Account from "./Account";
+import FreeComponent from "./FreeComponent";
+import AuthComponent from "./AuthComponent";
+import ProtectedRoutes from "./ProtectedRoutes";
+
+function App() {
+  return (
+    <Container>
+      <Row>
+        <Col className="text-center">
+          <h1>React Authentication Tutorial</h1>
+
+          <section id="navigation">
+            <a href="/">Home</a>
+            <a href="/free">Free Component</a>
+            <a href="/auth">Auth Component</a>
+          </section>
+        </Col>
+      </Row>
+
+      {/* create routes here */}
+      <Switch>
+        <Route exact path="/" component={Account} />
+        <Route exact path="/free" component={FreeComponent} />
+        <ProtectedRoutes path="/auth" component={AuthComponent} />
+      </Switch>
+    </Container>
+  );
+}
+
+export default App;
